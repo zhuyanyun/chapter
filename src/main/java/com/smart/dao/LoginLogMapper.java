f50d9d611119904f8ed22309f88e872c1aeaf933
zhuyanyun <yanyunzhu@newbanker.cn>
2019-09-09 08:46:39 +0800

    逆向工程

---
+++ src/main/java/com/smart/dao/LoginLogMapper.java
@@ -0,0 +1,17 @@
+package com.smart.dao;
+
+import com.smart.entity.LoginLog;
+
+public interface LoginLogMapper {
+    int deleteByPrimaryKey(Integer loginLogId);
+
+    int insert(LoginLog record);
+
+    int insertSelective(LoginLog record);
+
+    LoginLog selectByPrimaryKey(Integer loginLogId);
+
+    int updateByPrimaryKeySelective(LoginLog record);
+
+    int updateByPrimaryKey(LoginLog record);
+}
