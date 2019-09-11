faa997ba1005b1cb6b9c9dfe7e4660ba2a709df8
zhuyanyun <yanyunzhu@newbanker.cn>
2019-09-11 17:54:02 +0800

    security4

--- src/main/java/com/smart/controller/ReadingListController.java
+++ src/main/java/com/smart/controller/ReadingListController.java
@@ -1,8 +1,9 @@
 package com.smart.controller;
 
-import com.smart.entity.Book;
-import com.smart.service.ReadingListRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
+import com.smart.entity.Book;
+import com.smart.service.ReadingListRepository;
 
 /**
  * Created by zyy on 19/9/9.
@@ -18,16 +20,25 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix = "amazon")
 public class ReadingListController {
+
+    private String associatedId;
 
     @Autowired
     private ReadingListRepository readingListRepository;
+
+    public void setAssociatedId(String associatedId) {
+        this.associatedId = associatedId;
+    }
 
     @RequestMapping(value = "/{reader}",method = RequestMethod.GET)
     public String readersBooks(@PathVariable("reader") String reader, Model model){
         List<Book> readerList = readingListRepository.findByReader(reader);
         if(CollectionUtils.isEmpty(readerList)){
             model.addAttribute("books",readerList);
+            model.addAttribute("reader",reader);
+            model.addAttribute("amazonID", associatedId);
         }
         return "readingList";
     }
