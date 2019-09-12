c275094490f6f77947da534d5cb140073e5fa81d
zhuyanyun <yanyunzhu@newbanker.cn>
2019-09-12 18:36:58 +0800

    mock

---
+++ src/test/java/com/smart/mock/MockMvcWebTests.java
@@ -0,0 +1,21 @@
+package com.smart.mock;
+
+import com.smart.controller.ReadingListController;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @Description TODO
+ * @Author zhuyanyun
+ * @Date 2019-09-12 17:22
+ * @Vertion 1.0
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+//@SpringApplicationConfiguration(classes = ReadingListController.class)
+//@WebAppConfiguration
+public class MockMvcWebTests {
+
+}
